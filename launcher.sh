e6f4295d1b77c2a33e760def062b028e3479f0d0
Neil Jacobson <neil@formidableengineeringconsultants.com>
2017-12-11 21:06:10 -0800

    Added two utility routines that are used to initialize RPi pins at boot time I added them here because I wanted them in git somewhere

---
+++ launcher.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# launcher.sh
+#
+cd /
+cd home/pi
+sudo python pin_toggler.py &
+cd /
+#cd home/pi/pi-i2c
+#sleep 5
+#sudo ./rovdrv &
+#cd /
+#cd home/pi/openrov-cockpit
+#sleep 5
+#sudo PLATFORM=raspberrypi node src/cockpit.js &
+#cd /
---
+++ pin_toggler.py
@@ -0,0 +1,54 @@
+#
+# This starts at boot time of RPi through crontab
+# It initializes the pins of the RPi and gets the system
+# ready to run.
+# The script is started by launcher.sh
+# The application may override the initial pin settings 
+# indicated here.
+#
+# Import the required module. 
+import time
+# Get the RPi GPIO module
+import RPi.GPIO as GPIO 
+# Set the mode of numbering the pins. 
+GPIO.setmode(GPIO.BCM) 
+# GPIO pin 6 is an output. 
+GPIO.setup(6, GPIO.OUT) 
+# GPIO pin 17 is an output. 
+GPIO.setup(17, GPIO.OUT) 
+# GPIO pin 18 is an output. 
+GPIO.setup(18, GPIO.OUT) 
+# GPIO pin 19 is an output. 
+GPIO.setup(19, GPIO.OUT) 
+# GPIO pin 20 is an output. 
+GPIO.setup(20, GPIO.OUT) 
+# GPIO pin 21 is an output. 
+GPIO.setup(21, GPIO.OUT) 
+# GPIO pin 34 is an output
+GPIO.setup(34, GPIO.OUT)
+# GPIO pin 35 is an output
+GPIO.setup(35, GPIO.OUT)
+# Initialize 5V_OFF signals 
+GPIO.output(20, False) 
+GPIO.output(21, False) 
+# Initialize the system shutdown pin (STIR_OFF_Rx) to zero (GPIO34)
+GPIO.output(34, False) 
+# Initialise GPIO6 to high (true) to select the non-USB path
+GPIO.output(6, True) 
+# Initialise GPIO18 to high (true) to enable I2C
+GPIO.output(18, True) 
+# Pulse GPIO19 to reset I2C MUXes
+GPIO.output(19, False) 
+time.sleep(0.5)
+GPIO.output(19, True) 
+time.sleep(0.5)
+GPIO.output(19, False) 
+# Hold the Air Emergency Balast Valve 'blow' signal HIGH (GPIO35)
+GPIO.output(35, True)
+print "Toggling watchdog"
+while 1: 
+#    print "Watch"
+     time.sleep(0.250)
+     GPIO.output( 17, False) 
+     time.sleep(0.250)
+     GPIO.output( 17, True)
