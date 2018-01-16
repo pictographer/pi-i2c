a86309736021db999588a2e9fd3b3f7a964b1d10
Neil Jacobson <neil@formidableengineeringconsultants.com>
2018-01-15 16:00:56 -0800

    Add 'fake' program used to fool the system into thinking a remote camera is running

---
+++ remotecamera.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# This simple stub is used to make the system believe that a camera is alive and running
+# It should also ping the remote RPi and make sure it is still alive
+# It should also test the socket to make sure that is still alive
+#
+while : 
+do
+   sleep 10000000
+done
