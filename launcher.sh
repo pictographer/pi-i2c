441e8e8eda38233440f51c4fcb054b2902589464
Neil Jacobson <neil@formidableengineeringconsultants.com>
2018-02-05 14:38:10 -0800

    Update launcher script

--- launcher.sh
+++ launcher.sh
@@ -5,16 +5,16 @@ cd /
 cd home/pi
 sudo python pin_toggler.py &
 cd /
-#cd home/pi/pi-i2c
-#sleep 5
-#sudo ./rovdrv &
+cd home/pi/pi-i2c
+sleep 5
+sudo ./rovdrv &
 #If this is RPiA wait for RPiB to get its cameras up and running
-#if [ "$HOSTNAME" contains "stir01A" ]
-#then
-#    sleep 15
-#fi
-#cd /
-#cd home/pi/openrov-cockpit
-#sleep 5
-#sudo PLATFORM=raspberrypi node src/cockpit.js &
-#cd /
+if [ "$HOSTNAME" contains "stir01A" ]
+then
+    sleep 15
+fi
+cd /
+cd home/pi/openrov-cockpit
+sleep 5
+sudo PLATFORM=raspberrypi node src/cockpit.js &
+cd /
