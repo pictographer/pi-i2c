81fd157a72b73125a6e03ad7ac6eb5f1f80ff251
Neil Jacobson <neil@formidableengineeringconsultants.com>
2019-07-11 22:27:23 -0700

    Add log file to updater

--- updater.py
+++ updater.py
@@ -2,6 +2,7 @@ import os
 import os.path
 import tarfile
 import fnmatch
+import datetime
 from shutil import copyfile
 from shutil import rmtree
 from shutil import move
@@ -33,8 +34,11 @@ if os.path.exists(UPDATE_FILE) :
     GPIO.setup(12, GPIO.OUT)
     # turn on the LED flasher
     GPIO.output(12, True)
+    # Keep a record of update process and date
+    f = open(ROV_ROOT_DIR+"/update.log","a+")
+    f.write('System update executed at %s.\n' % (datetime.datetime.now()))
 
-    print("Update file exists: " + UPDATE_FILE)
+    f.write("Update file exists: " + UPDATE_FILE + "\n")
     # in case the update takes a while change the name
     # of the file so that the update procedure
     # is not triggered again
@@ -48,13 +52,13 @@ if os.path.exists(UPDATE_FILE) :
     out, err = p.communicate()
     for line in out.splitlines():
         if any(s in line for s in process_strings):
-            print(line)
+            f.write(line + "\n")
             pid = int(line.split()[1])
-            print("Killing PID: " + str(pid))
+            f.write("Killing PID: " + str(pid) + "\n")
             os.kill(pid, signal.SIGKILL)
 
     update_contents = UPDATE_WORK_FILE.split(".")[0]
-    print(update_contents)
+    f.write(update_contents + "\n")
     tarfile.open(UPDATE_WORK_FILE).extractall(update_contents)
     # now change directory to the context of the unpacked file
     os.chdir(update_contents)
@@ -68,57 +72,63 @@ if os.path.exists(UPDATE_FILE) :
             # saving off the original
             for filename in filenames:
                 source_file = os.path.join(dirpath, filename)
-                print( source_file )
+                f.write( source_file  + "\n")
                 if filename == "rovdrv" :
-                    print("Updating " + filename)
+                    f.write("Updating " + filename + "\n")
                     replace_file = os.path.join(PI_ROOT_DIR, filename)
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file,replace_file)
                     os.chmod(replace_file, 0o777)
                 elif filename == "pin_toggler.py" :
-                    print("Updating " + filename)
+                    f.write("Updating " + filename + "\n")
                     replace_file = os.path.join(ROV_ROOT_DIR, filename)
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file,replace_file)
                     os.chmod(replace_file, 0o777)
                 elif filename == "reboot_wifi.sh" :
-                    print("Updating " + filename)
+                    f.write("Updating " + filename + "\n")
                     replace_file = os.path.join(PI_ROOT_DIR, filename)
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file,replace_file)
                     os.chmod(replace_file, 0o777)
                 elif filename == "launcher.sh" :
-                    print("Updating " + filename)
+                    f.write("Updating " + filename + "\n")
                     replace_file = os.path.join(ROV_ROOT_DIR, filename)
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file,replace_file)
                     os.chmod(replace_file, 0o777)
+                elif filename == "Release_Notes.log" :
+                    f.write("Updating " + filename + "\n")
+                    replace_file = os.path.join(PI_ROOT_DIR, filename)
+                    if os.path.exists(replace_file):
+                        os.rename(replace_file, replace_file+".sv")
+                    copyfile(source_file,replace_file)
                 elif filename == "df.fs" :
-                    print("Updating " + filename)
+                    f.write("Updating " + filename + "\n")
                     replace_file = os.path.join(PI_ROOT_DIR, filename)
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file,replace_file)
                     os.chmod(replace_file, 0o777)
                 elif filename == "rovconfig.json" :
-                    print("Updating " + filename)
+                    f.write("Updating " + filename + "\n")
                     replace_file = os.path.join(ETC_DIR, filename)
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file,replace_file)
                     os.chmod(replace_file, 0o777)                    
                 else :
-                    print("Unexpected file: " + filename)
+                    f.write("Unexpected file: " + filename + "\n")
         elif dir == "openrov-cockpit" :
             for filename in filenames:
                 source_file = os.path.join(dirpath, filename)
-                print( os.path.join(dirpath, filename) )
+                f.write( os.path.join(dirpath, filename)  + "\n")
                 if filename == "openrov-cockpit.tar.gz" :
-                    print( "Updating " + filename)
+                    f.write( "Updating " + filename + "\n")
                     replace_file = os.path.join(ROV_ROOT_DIR, filename)
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
@@ -130,7 +140,7 @@ if os.path.exists(UPDATE_FILE) :
                             rmtree(original_dir+".sv", ignore_errors=True)
                         move(original_dir, original_dir+".sv")
                     # Now unzip the file
-                    print( "Untar " + replace_file)
+                    f.write( "Untar " + replace_file + "\n")
                     tarfile.open(replace_file).extractall(ROV_ROOT_DIR)
                     # Now chmod all the files
                     for root, dirs, files in os.walk(original_dir):  
@@ -139,7 +149,7 @@ if os.path.exists(UPDATE_FILE) :
                         for momo in files:
                             os.chmod(os.path.join(root, momo), 0o777)
                 else :
-                    print( "Unexpected file: " + filename)
+                    f.write( "Unexpected file: " + filename + "\n")
         elif dir == "mjpg_streamer" :
             # ensure that library directory exists
             if os.path.exists(LOCAL_LIB_DIR) is False:
@@ -147,16 +157,16 @@ if os.path.exists(UPDATE_FILE) :
             
             for filename in filenames:
                 source_file = os.path.join(dirpath, filename)
-                print( source_file )
+                f.write( source_file  + "\n")
                 if filename == "mjpg_streamer" :
                     replace_file = os.path.join(LOCAL_BIN_DIR, filename)
-                    print( "Updating " + replace_file)
+                    f.write( "Updating " + replace_file + "\n")
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file, replace_file)
                     os.chmod(replace_file, 0o777)
                 elif filename == "remotecamera.sh":
-                    print("Updating " + filename)
+                    f.write("Updating " + filename + "\n")
                     replace_file = os.path.join(LOCAL_BIN_DIR, filename)
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file + ".sv")
@@ -164,35 +174,35 @@ if os.path.exists(UPDATE_FILE) :
                     os.chmod(replace_file, 0o777)
                 elif filename == "input_raspicam.so":
                     replace_file = os.path.join(LOCAL_LIB_DIR, filename)
-                    print( "Updating " + replace_file)
+                    f.write( "Updating " + replace_file + "\n")
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file, replace_file)
                     os.chmod(replace_file, 0o777)
                 elif filename == "output_file.so":
                     replace_file = os.path.join(LOCAL_LIB_DIR, filename)
-                    print( "Updating " + replace_file)
+                    f.write( "Updating " + replace_file + "\n")
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file, replace_file)
                     os.chmod(replace_file, 0o777)
                 elif filename == "output_ws.so":
                     replace_file = os.path.join(LOCAL_LIB_DIR, filename)
-                    print( "Updating " + replace_file)
+                    f.write( "Updating " + replace_file + "\n")
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file, replace_file)
                     os.chmod(replace_file, 0o777)
                 elif filename == "output_http.so":
                     replace_file = os.path.join(LOCAL_LIB_DIR, filename)
-                    print( "Updating " + replace_file)
+                    f.write( "Updating " + replace_file + "\n")
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file, replace_file)
                     os.chmod(replace_file, 0o777)
                 elif filename == "libuv.so":
                     replace_file = os.path.join(LOCAL_LIB_DIR, filename+".2.0.0")
-                    print( "Updating " + replace_file)
+                    f.write( "Updating " + replace_file + "\n")
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file, replace_file)
@@ -220,7 +230,7 @@ if os.path.exists(UPDATE_FILE) :
                         os.remove(base_file+".2.0.0")
                 elif filename == "libssl.so":
                     replace_file = os.path.join(LOCAL_LIB_DIR, filename+".1.1")
-                    print( "Updating " + replace_file)
+                    f.write( "Updating " + replace_file + "\n")
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file, replace_file)
@@ -242,7 +252,7 @@ if os.path.exists(UPDATE_FILE) :
                         os.remove(base_file+".1.1")
                 elif filename == "libcrypto.so":
                     replace_file = os.path.join(LOCAL_LIB_DIR, filename+".1.0.0")
-                    print( "Updating " + replace_file)
+                    f.write( "Updating " + replace_file + "\n")
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file, replace_file)
@@ -264,7 +274,7 @@ if os.path.exists(UPDATE_FILE) :
                         os.remove(base_file+".1.0.0")
                 elif filename == "libuWS.so":
                     replace_file = os.path.join(LOCAL_LIB_DIR, filename)
-                    print( "Updating " + replace_file)
+                    f.write( "Updating " + replace_file + "\n")
                     if os.path.exists(replace_file):
                         os.rename(replace_file, replace_file+".sv")
                     copyfile(source_file, replace_file)
@@ -277,9 +287,9 @@ if os.path.exists(UPDATE_FILE) :
                     if os.path.exists(base_file) : 
                         os.remove(base_file)
                 else :
-                    print( "Unexpected file: " + filename)
+                    f.write( "Unexpected file: " + filename + "\n")
         else :
-            print( "Unexpected directory: " + dirpath)
+            f.write( "Unexpected directory: " + dirpath + "\n")
 
     # now we are done so delete the update file
     os.remove(UPDATE_WORK_FILE)
@@ -288,6 +298,7 @@ if os.path.exists(UPDATE_FILE) :
     rmtree(remove_dir)
     # turn off the LED flasher
     GPIO.output(12, False)
-
+    # close the log file
+    f.close()
 else :
     print("There is no update file: " + UPDATE_FILE)
